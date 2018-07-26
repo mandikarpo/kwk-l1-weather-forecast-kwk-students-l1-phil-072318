45c7293c767c59355ac4584fec97c46dd5478030
Mandi Karpo <mandi.karpo@gmail.com>
2018-07-26 13:55:41 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -10,13 +10,13 @@
 
 def location(weather)
 weather = {
-	:city => ["Philadelphia"],
-	:description => ["sunny with partial clouds"],
-	:temperature_farenheit => ["76"]
+	:city => "Philadelphia",
+	:description => "sunny with partial clouds",
+	:temperature_farenheit => "76"
 }
 end
 
-puts "In #{city}, it is #{description} with a temperature of #{temperature_farenheit}"
+puts "In #{:city}, it is #{:description} with a temperature of #{:temperature_farenheit}"
 
 # 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash. 
 
